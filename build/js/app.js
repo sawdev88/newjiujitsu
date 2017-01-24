2cc3865fd11309d7bef00a145d0b98bf0e444a88
Sean Wines <seanwinesdev@gmail.com>
2017-01-23 21:43:33 -0700

    Added About containers with display logic

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,43 @@
 'use strict';
+// var enterSite = prompt('Ener Password');
+var doc = document.querySelector('html');
+
+var aboutArea = document.querySelector('#about');
+var giTab = document.querySelector('.gi');
+var noGiTab = document.querySelector('.no-gi');
+var giContainer = document.querySelector('.gi-container');
+var noGiContainer = document.querySelector('.no-gi-container');
+
+var scroolToArea = function (section) {
+  section.scrollIntoView({
+    behavior: 'smooth'
+  });
+}
+
+// if (enterSite === 'newjits') {
+//   doc.style.display = 'block';
+// }
+
+giTab.addEventListener('click', function () {
+  // Give selected div active class
+  noGiTab.classList.remove('selected');
+  this.classList += ' selected';
+
+  // Give selcted tab's container focus
+  noGiContainer.classList.remove('active');
+  giContainer.classList += ' active';
+
+  scroolToArea(aboutArea);
+})
+
+noGiTab.addEventListener('click', function () {
+  // Give selected div active class
+  giTab.classList.remove('selected');
+  this.classList += ' selected';
+
+  // Give selcted tab's container focus
+  giContainer.classList.remove('active');
+  noGiContainer.classList += ' active';
+
+  scroolToArea(aboutArea);
+})
--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 
 gulp.task('es6', () => {
-  return gulp.src('src/app.js')
+  return gulp.src('/src/app.js')
       .pipe(babel({
         presets : ['es2015']
       }))
-      .pipe(gulp.dest('build/js'));
+      .pipe(gulp.dest('/build/js/app.js'));
 });
 
 gulp.task('default', ['es6'], () => {
